077325e6db47d11cc8e737d7539724eff84fe78f
hjcc27 <hilton.junio.costa@gmail.com>
2025-04-26 22:27:13 -0300

    Add: 6-aws-state-import-refresh

--- session-5/6-aws-state-import-refresh/s3.tf
+++ session-5/6-aws-state-import-refresh/s3.tf
@@ -6,13 +6,6 @@ resource "aws_s3_bucket" "bucket-2" {
   bucket = "hilton-tf-bucket-2"
 }
 
-resource "aws_s3_bucket" "bucket-3" {
+resource "aws_s3_bucket" "bucket-4" {
   bucket = "hilton-tf-bucket-3"
-}
-
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
