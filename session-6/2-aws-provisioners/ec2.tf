c8faf54814f82dc59632478b942abfa8e2101da7
hiltoncostacabral <hilton.costa.cabral@gmail.com>
2025-08-18 20:21:38 -0300

    Fix: session-6/2-aws-provisioners

--- session-6/2-aws-provisioners/ec2.tf
+++ session-6/2-aws-provisioners/ec2.tf
@@ -13,28 +13,38 @@ resource "aws_instance" "vpc" {
   tags = {
     Name = "hjcc-ec2-instance"
   }
-  
+
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} >> public_ips.txt"
+    command = "echo ${self.public_ip} >> /tmp/public_ips.txt"
   }
 
   connection {
     type        = "ssh"
-    user        = "ubuntu"
+    user        = "ec2-user"
     private_key = file("~/.ssh/id_rsa")
     host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "echo subnet_id ${data.terraform_remote_state.vpc.outputs.id_subnet_terraform} >> ~/subnet_ids.txt",
-      "echo security_groups ${data.terraform_remote_state.vpc.outputs.id_security_group_terraform} >> ~/securitygroup_ids.txt"
+      "echo subnet_id ${data.terraform_remote_state.vpc.outputs.id_subnet_terraform} >> /tmp/network_ids.txt",
+      "echo security_groups ${data.terraform_remote_state.vpc.outputs.id_security_group_terraform} >> /tmp/network_ids.txt"
     ]
   }
 
   provisioner "file" {
     source      = "./private_ips.txt"
-    destination = "~/ips.txt"
+    destination = "/tmp/ips.txt"
+  }
+
+  provisioner "file" {
+    content     = "ami used: ${self.ami}"
+    destination = "/tmp/ami.txt"
+  }
+
+  provisioner "file" {
+    content     = "instance type: ${self.instance_type}"
+    destination = "/tmp/instance_type.txt"
   }
 
 }
