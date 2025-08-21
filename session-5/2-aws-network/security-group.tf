83f7332df4435e7aa8fb8afb4805d43b7f708975
hiltoncostacabral <hilton.costa.cabral@gmail.com>
2025-08-21 10:18:19 -0300

    Add rule to egress to internet on security group

--- session-5/2-aws-network/security-group.tf
+++ session-5/2-aws-network/security-group.tf
@@ -15,3 +15,11 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  from_port         = 0
+  to_port           = 0
+}
