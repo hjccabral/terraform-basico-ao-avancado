371bfc41412ce7c221920c85cbf104a1d32ba25e
hjcc27 <hilton.junio.costa@gmail.com>
2025-08-29 11:46:38 -0300

    Fix: 2-aws-count-vpc

--- session-8/2-aws-count-vpc/output.tf
+++ session-8/2-aws-count-vpc/output.tf
@@ -1,4 +1,14 @@
-output "id_subnet_terraform" {
-  description = "value of subnet id"
-  value       = aws_subnet.subnet.id
+output "id_subnet_0_terraform" {
+  description = "value of subnet id 0"
+  value       = aws_subnet.subnet[0].id
+}
+
+output "id_subnet_1_terraform" {
+  description = "value of subnet id 1"
+  value       = aws_subnet.subnet[1].id
+}
+
+output "id_subnet_2_terraform" {
+  description = "value of subnet id 2"
+  value       = aws_subnet.subnet[2].id
 }
